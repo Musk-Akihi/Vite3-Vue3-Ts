9572b6ef36c7ad2207839b9a17db9911df08ea82
duxinwu <13935741214@163.com>
2022-08-03 15:34:50 +0800

    config: 配置全局scss文件

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,20 @@ import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import path from 'path'
 
-// https://vitejs.dev/config/
+// https://cn.vitejs.dev/config
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      // 局样式文件全局注入到项目中
+      scss: {
+        additionalData: '@import "@/assets/css/main.scss";'
+      }
     }
   }
 })
